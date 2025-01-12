c742b4d9f185ca857f985f20f0ffaef6e896db97
alijonsina <alijonsina9@gmail.com>
2025-01-12 17:07:40 +0100

    Delete Electronics_Store_Management_System directory

--- Electronics_Store_Management_System/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.electronics_store_management_system {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens com.example.electronics_store_management_system to javafx.fxml;
-    exports com.example.electronics_store_management_system;
-}
